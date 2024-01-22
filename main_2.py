b4f56ee5e34c85a9abbf509238cfca8117f2da79
rafsunsheikh <201614123@student.mist.ac.bd>
2024-01-22 21:18:25 +0600

    added secret

--- main_2.py
+++ main_2.py
@@ -16,8 +16,8 @@ from transformers import AutoTokenizer , AutoModelForCausalLM
 import torch 
 from langchain import PromptTemplate, LLMChain
 
-# huggingface_api_key = st.secrets["HUGGINGFACEHUB_API_KEY"]
-huggingface_api_key = "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"
+huggingface_api_key = st.secrets["HUGGINGFACEHUB_API_KEY"]
+# huggingface_api_key = "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"
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = huggingface_api_key
 
 
